675fbc0c19058d9c356c320f74ee1314fa153c45
p.bialkowski <p.bialkowski@theoutsourcing.pl>
2018-01-15 23:25:58 +0100

    Prevent memory leak, add virtual dtor to Item

--- include/item.h
+++ include/item.h
@@ -10,6 +10,7 @@ class Item
         virtual std::string name() const = 0; 
         virtual unsigned thickness() const = 0;
         virtual std::string info() const = 0; 
+        virtual ~Item() { }
 };
 
 #endif /* __ITEM_H__ */
